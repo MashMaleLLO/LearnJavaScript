8bde1ed72e744160fcfc786e08b98a375d8a00cf
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-08-17 17:56:39 +0700

    try login and register with express gen

--- ExpressGenerator/my-app/routes/index.js
+++ ExpressGenerator/my-app/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get("/login", (req,res)=> 
+{
+  res.render('login')
+})
+
+router.get("/register", (req,res)=> 
+{
+  res.render('register')
+})
+
 module.exports = router;
