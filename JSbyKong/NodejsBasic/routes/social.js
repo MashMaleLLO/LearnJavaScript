f16f27fc75544a2534f021f6916a319df9aeed6f
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-08-22 22:50:41 +0700

    add social router and social template.

---
+++ JSbyKong/NodejsBasic/routes/social.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => 
+{
+    res.render('social')
+})
+
+router.get('/Q&A', (req, res, next) => 
+{
+    res.send('Q and A page.')
+})
+
+router.get('/gallery', (req, res, next) => 
+{
+    res.send('This our gallery page.')
+})
+
+module.exports = router
