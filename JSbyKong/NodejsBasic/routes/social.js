42d8700456d92e1509743e5f4b3b757abbcc4bcc
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-08-24 19:57:43 +0700

    test post method and add QandA form.

--- JSbyKong/NodejsBasic/routes/social.js
+++ JSbyKong/NodejsBasic/routes/social.js
@@ -11,6 +11,19 @@ router.get('/Q&A', (req, res, next) =>
     res.render('qAnda')
 })
 
+router.get('/Q&A/addQuestion', (req, res, next) => 
+{
+    res.render('addQuestion')
+})
+
+router.post('/Q&A/addQuestion', (req, res, next) => 
+{
+    console.log(req.body.name)
+    console.log(req.body.detail)
+    console.log(req.body.tag)
+    res.render('addQuestion')
+})
+
 router.get('/gallery', (req, res, next) => 
 {
     res.send('This our gallery page.')
