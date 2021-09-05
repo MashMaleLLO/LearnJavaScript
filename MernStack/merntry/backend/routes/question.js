30409ac835bac69cf6d14ef4894a0815d9ec46d4
MashMaleLLO <74470396+MashMaleLLO@users.noreply.github.com>
2021-09-05 18:01:59 +0700

    add CRUD for question.

--- MernStack/merntry/backend/routes/question.js
+++ MernStack/merntry/backend/routes/question.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const question = require('../model/postModel')
 const router = express.Router()
 let Question = require('../model/postModel')
 
@@ -23,5 +24,36 @@ router.post('/add', (req, res) =>
     .then(() => {res.json('Question add')})
     .catch(err => { res.status(400).json('Error: ' + err) } )
 })
+
+router.get('/:id', (req, res) => 
+{
+    Question.findById(req.params.id)
+    .then(question => { res.json(question) })
+    .catch(err => {res.status(400).json('Error: ' + err)})
+})
+
+router.delete('/:id', (req, res) => 
+{
+    Question.findByIdAndDelete(req.params.id)
+    .then(() => { res.json('Question deleted.') })
+    .catch(err => { res.status(400).json('Error: ' + err) })
+})
+
+router.post('/update/:id', (req, res) => 
+{
+    Question.findById(req.params.id)
+    .then(question => 
+        {
+            question.username = req.body.username
+            question.description = req.body.description
+            question.date = Date.parse(req.body.date)
+
+            question.save()
+            .then(() => { res.json("Question updated") })
+            .catch(err => { res.status(400).json('Error: ' + err) })
+        })
+    .catch(err => { res.status(400).json('Error: ' + err) })
+})
+
 
 module.exports = router
